242fbf1f5553c83bbe0a610c0c30631c8ecdda42
stephenmk <stephenmk@users.noreply.github.com>
2022-04-14 16:05:59 -0500

    Initial commit

---
+++ corpus_count_percentages.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name        edrdg N-gram Corpus Count Percentages
+// @namespace   edrdg-scripts
+// @version     1.0
+// @author      Stephen Kraus
+// @match       *://*.edrdg.org/~jwb/cgi-bin/ngramlookup*
+// @icon        https://www.edrdg.org/favicon.ico
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+'use strict';
+
+
+function main() {
+	// Calculate the total number of counts (for calculating percentages later).
+	// Determine the maximum text length (for padding and formatting later).
+	let totalCount = 0;
+	let maxCountCellLength = 0;
+	document.querySelectorAll("tr td:nth-child(2)").forEach(cell => {
+		const cellCount = parseInt(cell.innerText);
+		totalCount += !Number.isNaN(cellCount) ? cellCount : 0;
+		if (maxCountCellLength < cell.innerText.length) {
+			maxCountCellLength = cell.innerText.length;
+		}
+	})
+
+
+	// Prepend new cells to the table with percentage information.
+	document.querySelectorAll("tr").forEach(row => {
+		const cellCount = parseInt(row.cells[1].innerText);
+		const percText = !Number.isNaN(cellCount) ?
+			parseFloat(cellCount * 100.0 / totalCount).toFixed(2) + "%" :
+			"-  ";
+		// Pad with spaces so this info will look nicer when copied & pasted elsewhere.
+		const percTextNode = document.createTextNode(percText.padStart(6));
+		const percCell = row.insertCell(0);
+		percCell.appendChild(percTextNode);
+	})
+
+
+	// Move the count cells to the beginning of the rows.
+	document.querySelectorAll("tr").forEach(row => {
+		const countNodeCopy = document.createTextNode(row.cells[2].innerText.padStart(maxCountCellLength));
+		const newCountCell = row.insertCell(0);
+		newCountCell.appendChild(countNodeCopy);
+		row.deleteCell(3);
+	})
+
+
+	// Align the counts and percentages to the right.
+	const style = document.createElement('style');
+	style.innerHTML = `
+          tr td:nth-child(-n+2) {
+            white-space: pre;
+            text-align: right;
+            padding-right: 25px;
+          }`;
+	document.head.appendChild(style);
+}
+
+
+// Starting the program this way prevents it from
+// running again on return visits to cached pages
+// (when running the program via greasemonkey).
+window.addEventListener("load", main, false);
---
+++ jmdictdb_ext_link_menu.js
@@ -0,0 +1,178 @@
+// ==UserScript==
+// @name        JMdictDB external links
+// @namespace   edrdg-scripts
+// @version     1.0
+// @author      Stephen Kraus
+// @match       *://*.edrdg.org/jmdictdb/cgi-bin/updates.py*
+// @match       *://*.edrdg.org/jmdictdb/cgi-bin/entr.py*
+// @match       *://*.edrdg.org/jmdictdb/cgi-bin/edconf.py*
+// @icon        https://www.edrdg.org/favicon.ico
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+'use strict';
+
+
+const urls = {
+	"google":   "https://www.edrdg.org/~jwb/cgi-bin/ngramlookup?sent=",
+	"km":       "https://www.edrdg.org/~jwb/cgi-bin/ngramlookupwww?sent=",
+	"kotobank": "https://kotobank.jp/gs/?q=",
+	"eijiro":   "https://eow.alc.co.jp/search?q=",
+	"wadoku":   "https://www.wadoku.de/search/?q="
+}
+
+
+function makeLinkMenuStyleClasses() {
+	const style = document.createElement('style');
+	style.innerHTML = `
+          .linkMenuContainer {
+            position: relative;
+            display: inline-block;
+          }
+          .linkMenuButton {}
+          .linkMenuContent {
+            display: none;
+            border-style: ridge;
+            border-radius: 5px;
+            position: absolute;
+            background-color: #feffef;
+            min-width: 210px;
+            box-shadow: 0px 20px 20px 0px rgba(0,0,0,0.5);
+            z-index: 1;
+          }
+          .linkMenuItem {
+            display: block;
+            padding: 2px 0px 2px 20px;
+          }
+          .linkMenuHeading {
+            display: block;
+            padding: 2px 0px 2px 10px;
+          }
+          .showLinkMenuContent {
+            display: block;
+          }`;
+	document.head.appendChild(style);
+}
+
+
+// Toggle the display of the link menu on button clicks
+function linkMenuButtonClick(event) {
+	const linkMenuContent = document.getElementById(event.currentTarget.linkMenuContentId);
+	const doShowMenu = !linkMenuContent.classList.contains("showLinkMenuContent");
+	hideAllLinkMenus();
+	if (doShowMenu) {
+		linkMenuContent.classList.add("showLinkMenuContent")
+	}
+}
+
+
+// Hide the menu after an outside click
+function makeLinkMenuHideListener() {
+	const listener = (event) => {
+		let isLinkMenu = false;
+		event.target.classList.forEach(className => {
+			if (className.startsWith("linkMenu")) {
+				isLinkMenu = true;
+			}
+		})
+		if (isLinkMenu) return;
+		hideAllLinkMenus();
+	}
+	document.addEventListener("click", listener, false)
+}
+
+
+function hideAllLinkMenus() {
+	document.querySelectorAll(".showLinkMenuContent").forEach(element => {
+		element.classList.remove('showLinkMenuContent')
+	})
+}
+
+
+function makeLink(innerText, url, parameterList) {
+	const textNode = document.createTextNode(innerText);
+	const linkElement = document.createElement("a");
+	linkElement.appendChild(textNode);
+	linkElement.href = url + parameterList.join('+');
+	linkElement.classList.add("linkMenuItem");
+	return linkElement;
+}
+
+
+function makeLinkHeading(innerText) {
+	const textNode = document.createTextNode("[" + innerText + "]");
+	const spanNode = document.createElement("span");
+	spanNode.appendChild(textNode)
+	spanNode.classList.add("linkMenuHeading");
+	return spanNode;
+}
+
+
+function makeLinkMenus() {
+	document.querySelectorAll(".item").forEach((item, index) => {
+		const kanjiList = [];
+		item.querySelectorAll(".kanj").forEach(kanji => {
+			kanjiList.push(kanji.innerText)
+		})
+
+		const readingList = [];
+		item.querySelectorAll(".rdng").forEach(reading => {
+			readingList.push(reading.innerText)
+		})
+
+		const allExpressions = kanjiList.concat(readingList);
+		if (allExpressions.length == 0) return;
+
+		// Display links for all kanji expressions and the first reading.
+		// If there are no kanji expressions, use all the readings.
+		const linkExpressions = (kanjiList.length != 0) ?
+			kanjiList.concat(readingList[0]) :
+			readingList;
+
+		const menuItems = [
+			makeLink("N-gram counts (Google)", urls["google"], allExpressions),
+			makeLink("N-gram counts (KM)", urls["km"], allExpressions)
+		]
+		linkExpressions.forEach(expression => {
+			menuItems.push(makeLinkHeading(expression))
+			menuItems.push(makeLink("Kotobank", urls["kotobank"], [expression]))
+			menuItems.push(makeLink("Eijiro (ALC server)", urls["eijiro"], [expression]))
+			menuItems.push(makeLink("Wadoku", urls["wadoku"], [expression]))
+		})
+
+		const linkMenuContentId = "linkMenu" + index
+		const linkMenuContent = document.createElement("div");
+		linkMenuContent.id = linkMenuContentId
+		linkMenuContent.classList.add("linkMenuContent");
+		menuItems.forEach(menuItem => {
+			linkMenuContent.appendChild(menuItem);
+		})
+
+		const linkMenuButton = document.createElement("button");
+		linkMenuButton.classList.add("linkMenuButton");
+		linkMenuButton.innerText = "External Links"
+		linkMenuButton.linkMenuContentId = linkMenuContentId
+		linkMenuButton.addEventListener("click", linkMenuButtonClick, false)
+
+		const linkMenuContainer = document.createElement("div");
+		linkMenuContainer.classList.add("linkMenuContainer");
+		linkMenuContainer.appendChild(linkMenuButton);
+		linkMenuContainer.appendChild(linkMenuContent);
+
+		const firstLineBreak = item.querySelector("br");
+		firstLineBreak.before(linkMenuContainer);
+	})
+}
+
+
+function main() {
+	makeLinkMenuStyleClasses();
+	makeLinkMenus()
+	makeLinkMenuHideListener();
+}
+
+
+// Starting the program this way prevents it from
+// running again on return visits to cached pages
+// (when running the program via greasemonkey).
+window.addEventListener("load", main, false);
