ee20982103b4875088dd6f92e055ca279410fad8
stephenmk <stephenmk@users.noreply.github.com>
2022-06-01 16:12:54 -0500

    New version of N-gram Corpus Count Percentages
    * Add checkboxes to enable/disable percentages for each term
    * Add a toggle button to enable/disable percentages globally
    * Add a 'Copy to Clipboard' button

--- corpus_count_percentages.user.js
+++ corpus_count_percentages.user.js
@@ -1,66 +1,160 @@
 // ==UserScript==
 // @name        edrdg N-gram Corpus Count Percentages
 // @namespace   edrdg-scripts
-// @version     1.1
+// @version     1.2
 // @author      Stephen Kraus
 // @match       *://*.edrdg.org/~jwb/cgi-bin/ngramlookup*
 // @icon        https://www.edrdg.org/favicon.ico
-// @grant       none
+// @grant       GM_setClipboard
 // @run-at      document-end
 // ==/UserScript==
 'use strict';
 
 
-function main() {
-	// Calculate the total number of counts (for calculating percentages later).
-	// Prepend formatted count text values to the beginning of the rows and
-	// determine the maximum text length (for padding later).
+const WORD_COL = 0;
+const COUNT_COL = 1;
+const PERC_COL = 2;
+
+
+function tableToText() {
+	let includePercentages = false;
+
+	document.querySelectorAll("tr").forEach(row => {
+		const checkbox = row.querySelector("INPUT");
+		if (checkbox.checked) {
+			includePercentages = true;
+		}
+	})
+
+	const maxWordLength = calculateMaxLength(WORD_COL);
+	const maxCountCellLength = calculateMaxLength(COUNT_COL);
+	let text = document.querySelector("h2").innerText + "\n";
+
+	document.querySelectorAll("tr").forEach(row => {
+		const wordCell = row.cells[WORD_COL];
+		const countCell = row.cells[COUNT_COL];
+		const percCell = row.cells[PERC_COL];
+		const checkbox = row.querySelector("INPUT");
+
+		text += "| "
+		text += wordCell.innerText.padEnd(maxWordLength, "　");
+		text += " | "
+		text += countCell.innerText.padStart(maxCountCellLength);
+		text += " |"
+
+		if (includePercentages && checkbox.checked) {
+			text += percCell.innerText.padStart(6) + " |";
+		} else if (includePercentages) {
+			text += "  N/A% |";
+		}
+
+		text += "\n";
+	})
+
+	return text.substring(0, text.length - 1);;
+}
+
+
+function copyTableToClipboard() {
+	const tableText = tableToText();
+	GM_setClipboard(tableText);
+}
+
+
+function calculateMaxLength(column) {
+	let maxLength = 0;
+	document.querySelectorAll("tr").forEach(row => {
+		const cell = row.cells[column];
+		const length = cell.innerText.length;
+		if (maxLength < length) {
+			maxLength = length;
+		}
+	})
+	return maxLength;
+}
+
+
+function displayPercentages() {
 	let totalCount = 0;
-	let maxCountCellLength = 0;
 	document.querySelectorAll("tr").forEach(row => {
-		const cellCount = parseInt(row.cells[1].innerText);
-		const displayCell = row.insertCell(0);
-
-		if (!Number.isNaN(cellCount)) {
-			totalCount += cellCount;
-			displayCell.innerText = Number(cellCount).toLocaleString();
+		const checkbox = row.querySelector("INPUT");
+		if (checkbox.checked) {
+			const countCell = row.cells[COUNT_COL];
+			totalCount += parseInt(countCell.dataset.count);
+		}
+	})
+	document.querySelectorAll("tr").forEach(row => {
+		const checkbox = row.querySelector("INPUT");
+		const countCell = row.cells[COUNT_COL];
+		const percCell = row.cells[PERC_COL];
+		if (checkbox.checked) {
+			const cellCount = parseInt(countCell.dataset.count);
+			const percText = parseFloat(cellCount * 100.0 / totalCount).toFixed(1) + "%";
+			percCell.innerText = percText;
 		} else {
-			displayCell.innerText = "None";
-		}
-
-		if (maxCountCellLength < displayCell.innerText.length) {
-			maxCountCellLength = displayCell.innerText.length;
+			percCell.innerText = "";
 		}
 	})
+}
 
 
-	// Insert new cells into the table with percentage information.
-	// Format the new count cells and delete the original cells.
+function togglePercentages() {
+	const firstCheckBox = document.querySelector("INPUT");
+	const toggle = !firstCheckBox.checked;
 	document.querySelectorAll("tr").forEach(row => {
-		const cellCount = parseInt(row.cells[2].innerText);
-		const percText = !Number.isNaN(cellCount) ?
-			parseFloat(cellCount * 100.0 / totalCount).toFixed(1) + "%" :
-			"-  ";
-
-		// Pad percentages with spaces so they'll look nicer when copied & pasted elsewhere.
-		const percCell = row.insertCell(1);
-		percCell.innerText = percText.padStart(6);
-
-		// Pad formatted counts with spaces.
-		row.cells[0].innerText = row.cells[0].innerText.padStart(maxCountCellLength);
-
-		// Delete the original count cells.
-		row.deleteCell(3);
+		const checkbox = row.querySelector("INPUT");
+		checkbox.checked = toggle;
 	})
+	displayPercentages();
+}
 
 
-	// Align the counts and percentages to the right.
+function formatCounts() {
+	document.querySelectorAll("tr").forEach(row => {
+		const countCell = row.cells[COUNT_COL];
+		const cellCount = parseInt(countCell.innerText);
+		if (!Number.isNaN(cellCount)) {
+			countCell.dataset.count = cellCount;
+			countCell.innerText = Number(cellCount).toLocaleString();
+		} else {
+			countCell.dataset.count = 0;
+			countCell.innerText = "0";
+		}
+	})
+}
+
+
+function main() {
+	formatCounts();
+
+	document.querySelectorAll("tr").forEach(row => {
+		while (row.cells.length < 3) {
+			const newCell = row.insertCell();
+			newCell.align = "right";
+		}
+		const checkBox = document.createElement("INPUT");
+		checkBox.setAttribute("type", "checkbox");
+		checkBox.checked = true;
+		checkBox.addEventListener('change', displayPercentages);
+		const checkboxCell = row.insertCell();
+		checkboxCell.appendChild(checkBox);
+	})
+	displayPercentages();
+
+	const toggleButton = document.createElement("button");
+	toggleButton.innerText = "Toggle Percentages";
+	toggleButton.addEventListener('click', togglePercentages);
+	document.body.appendChild(toggleButton);
+
+	const copyButton = document.createElement("button");
+	copyButton.innerText = "Copy to Clipboard";
+	copyButton.addEventListener('click', copyTableToClipboard);
+	document.body.appendChild(copyButton);
+
 	const style = document.createElement('style');
-	style.innerHTML = `
-          tr td:nth-child(-n+2) {
-            white-space: pre;
-            text-align: right;
-            padding-right: 25px;
+	style.innerText = `
+          tr td:nth-child(3) {
+            min-width: 60px;
           }`;
 	document.head.appendChild(style);
 }
